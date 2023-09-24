0135e7bafe615b6fc5f0f895f92059d2d42d2a5d
ayanonline <ayanghosh1389@gmail.com>
2023-09-24 23:50:52 +0530

    feat: Make offer seciton responsive for mobile

--- src/components/OfferCard.jsx
+++ src/components/OfferCard.jsx
@@ -1,12 +1,14 @@
 const OfferCard = ({ img, price, title }) => {
   return (
-    <div className="flex h-56 rounded-md bg-[#ffe9e8] px-2 py-6">
-      <img src={img} alt="" className="h-[10rem] object-contain" />
-      <div className="flex flex-col items-center justify-between">
-        <h1 className="text-4xl">{title}</h1>
-        <div>
-          <span className="text-green-500">From {price}</span>
-          <button className="ml-4 rounded-md bg-green-500 px-6 py-2 text-white">
+    <div className="flex w-1/2 flex-col justify-between rounded-md bg-[#ffe9e8] p-2 lg:h-56 lg:w-full lg:flex-row lg:px-2 lg:py-6">
+      <img src={img} alt="" className="h-[4rem] object-contain lg:h-[10rem]" />
+      <div className="flex flex-col items-center justify-between lg:items-center">
+        <h1 className="text-xs lg:text-4xl">{title}</h1>
+        <div className="flex flex-col items-center justify-between">
+          <span className="text-xs text-green-500 lg:text-base">
+            From {price}
+          </span>
+          <button className="rounded-md bg-green-500 px-3 py-2 text-xs text-white lg:ml-4 lg:px-6 lg:text-base">
             Shop Now
           </button>
         </div>
--- src/components/OfferSection.jsx
+++ src/components/OfferSection.jsx
@@ -2,23 +2,23 @@ import OfferCard from "./OfferCard";
 
 const OfferSection = () => {
   return (
-    <section className="mx-[20rem] flex gap-8">
-      <div className="relative h-[30rem] w-1/2 rounded-md bg-[#f0ffdf]">
-        <span className="absolute right-10  bg-[#ffdb8e] px-4 py-8 text-5xl leading-normal">
+    <section className="mx-5 flex flex-col gap-2 lg:mx-[20rem] lg:flex-row lg:gap-8">
+      <div className="relative h-[15rem] rounded-md bg-[#f0ffdf] lg:h-[30rem] lg:w-1/2">
+        <span className="absolute right-2 bg-[#ffdb8e] p-1 text-base  leading-normal lg:right-10 lg:px-4 lg:py-8 lg:text-5xl">
           Sale <br />
           30% <br /> off
         </span>
-        <p className="p-5 text-[3rem] font-thin">
+        <p className="p-2 text-xl font-thin lg:p-5 lg:text-[3rem]">
           <span>Farm</span> <span className="text-green-500">Fresh</span>
           <br /> Vegetable
         </p>
         <img
           src="/vegetables2.png"
           alt="offer image"
-          className="absolute bottom-0 h-[24rem] rounded-bl-md"
+          className="absolute bottom-0 h-[12rem] rounded-bl-md lg:h-[24rem]"
         />
       </div>
-      <div className="flex w-1/2 flex-col justify-between">
+      <div className="flex justify-between gap-2 lg:w-1/2 lg:flex-col">
         <OfferCard
           img="/meats2.png"
           title="Upto 13% off Fresh Meat"
