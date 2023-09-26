3414bbffc8af11f368208da7be907262a6208de9
ayanonline <ayanghosh1389@gmail.com>
2023-09-26 14:28:18 +0530

    fix: Increase padding for offer section

--- src/components/OfferSection.jsx
+++ src/components/OfferSection.jsx
@@ -4,11 +4,11 @@ const OfferSection = () => {
   return (
     <section className="mx-5 flex flex-col gap-2 lg:mx-[20rem] lg:flex-row lg:gap-8">
       <div className="relative h-[15rem] rounded-md bg-[#f0ffdf] lg:h-[30rem] lg:w-1/2">
-        <span className="absolute right-2 bg-[#ffdb8e] p-1 text-base  leading-normal lg:right-10 lg:px-4 lg:py-8 lg:text-5xl">
+        <span className="absolute right-6 bg-[#ffdb8e] px-4 py-10 text-base  leading-normal lg:right-10 lg:px-4 lg:py-8 lg:text-5xl">
           Sale <br />
           30% <br /> off
         </span>
-        <p className="p-2 text-xl font-thin leading-normal lg:p-5 lg:text-[3rem]">
+        <p className="p-2 text-2xl font-thin leading-normal lg:p-5 lg:text-[3rem]">
           <span>Farm</span> <span className="text-green-500">Fresh</span>
           <br /> Vegetable
         </p>
