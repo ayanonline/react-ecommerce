53c196667e365811c75d1c67cb6c172f0308e7a0
ayanonline <ayanghosh1389@gmail.com>
2023-09-26 12:51:29 +0530

    feat: Make subscribe section responsive for mobile

--- src/components/SubscribeSection.jsx
+++ src/components/SubscribeSection.jsx
@@ -1,17 +1,17 @@
 const SubscribeSection = () => {
   return (
-    <section className="bg-green-100 py-10 text-center">
-      <h1 className="text-5xl">Get The Newest Offers</h1>
-      <p className="my-8 text-stone-500">
+    <section className="bg-green-100 py-4 text-center lg:py-10">
+      <h1 className="text-lg lg:text-5xl">Get The Newest Offers</h1>
+      <p className="my-2 text-xs text-stone-500 lg:my-8 lg:text-base">
         and a coupon for 20% off your first purchase
       </p>
       <div className="mx-auto w-fit rounded-md border-2">
         <input
           type="text"
-          placeholder="Enter your email address"
-          className="rounded-l-md p-3 outline-none"
+          placeholder="Your email address"
+          className="rounded-l-md px-2 py-1 outline-none lg:p-3"
         />
-        <button className="rounded-r-md bg-green-500 px-6 py-3 text-white">
+        <button className="rounded-r-md bg-green-500 px-2 py-1 text-white lg:px-6 lg:py-3">
           Subscribe
         </button>
       </div>
