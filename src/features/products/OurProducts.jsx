50f02d4a4a41074e51efed7ef65d28523a579433
ayanonline <ayanghosh1389@gmail.com>
2023-09-26 12:37:52 +0530

    feat: Make our-product section responsive for mobile

--- src/features/products/OurProducts.jsx
+++ src/features/products/OurProducts.jsx
@@ -14,9 +14,9 @@ const OurProducts = () => {
   const { isLoading, productsData, error } = useProducts(6);
 
   return (
-    <section className="mx-[20rem] py-8">
-      <h1 className="mb-5 text-5xl">Our Products</h1>
-      <div className="relative">
+    <section className="mx-5 py-4 lg:mx-[20rem] lg:py-8">
+      <h1 className="lg:mb-5 lg:text-5xl">Our Products</h1>
+      <div className="relative hidden lg:block">
         <div className="absolute top-1/2 z-0 h-[1px] w-full bg-green-500"></div>
         <div className="relative z-20 flex justify-between">
           <Button title="All Items" />
@@ -27,13 +27,13 @@ const OurProducts = () => {
           <Button title="Bread & cakes" />
         </div>
       </div>
-      <div className="my-4 flex flex-wrap justify-between">
+      <div className="flex flex-wrap justify-between lg:my-4">
         {!isLoading &&
           productsData.products.map((item) => (
             <ProductCard key={item._id} product={item} />
           ))}
       </div>
-      <button className="mx-auto flex rounded-md bg-green-500 px-8 py-4 text-xl text-white">
+      <button className="mx-auto mt-4 flex rounded-md bg-green-500 px-3 py-2 text-sm text-white lg:px-8 lg:py-4 lg:text-xl">
         <Link to="/products">view all products</Link>
       </button>
     </section>
--- src/features/products/ProductCard.jsx
+++ src/features/products/ProductCard.jsx
@@ -6,30 +6,30 @@ const ProductCard = ({ product }) => {
   const { isAddingToCart, addToCart } = useAddToCart(product._id);
 
   return (
-    <div className="my-2 flex w-[25rem] flex-col items-center gap-4 rounded-md border px-10 py-4 shadow-lg">
+    <div className="my-2 flex w-[10.5rem] flex-col items-center gap-1 rounded-md border p-4 shadow-lg lg:w-[25rem] lg:gap-4">
       <Link to={"/products/" + product._id}>
         <img
           src={product.thumbnail}
           alt="product image"
-          className="h-[15rem] w-[20rem] border object-cover"
+          className="h-[7rem] w-[8rem] border object-cover lg:h-[15rem] lg:w-[20rem]"
         />
       </Link>
-      <h1 className="text-3xl">{product.name}</h1>
+      <h1 className="text-sm lg:text-3xl">{product.name}</h1>
       <div className="flex items-center">
         <div className="mr-1 flex items-center">
-          <AiFillStar className="mr-1 text-xl text-green-600" />
+          <AiFillStar className="mr-1 text-xs text-green-600 lg:text-xl" />
           {product.ratings}.0
         </div>
         ({product.numOfReviews})
       </div>
 
-      <p className="text-lg">
+      <p className="text-xs lg:text-lg">
         <span>{product.quantity}</span>
         <span className="ml-4 text-green-500">₹{product.price}</span>
       </p>
 
       <button
-        className="rounded-md border border-black px-20 py-4 text-xl hover:border-none hover:bg-green-500 hover:text-white"
+        className="rounded-md border border-black px-3 py-2 text-xs hover:border-none hover:bg-green-500 hover:text-white lg:px-20 lg:py-4 lg:text-xl"
         onClick={addToCart}
         disabled={isAddingToCart}
       >
