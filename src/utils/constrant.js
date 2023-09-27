62d8a42b3f373d9e6f77d0ef8fd5093d74c681d9
ayanonline <ayanghosh1389@gmail.com>
2023-09-27 21:19:59 +0530

    baseUrl changed

--- src/utils/constrant.js
+++ src/utils/constrant.js
@@ -1,5 +1,5 @@
 export const baseUrl = "https://freshgrocery.onrender.com/api/v1";
-export const domain = "https://your-freshgrocery.netlify.app/";
+export const domain = "your-freshgrocery.netlify.app";
 
 // export const baseUrl = "http://localhost:4000/api/v1";
 // export const domain = "localhost";
