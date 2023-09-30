de92b70618c2336d568796651e83ce8cef4a3d94
kartick17 <karticksadhu0@gmail.com>
2023-09-30 17:07:04 +0530

    Modify server base url

--- src/utils/constrant.js
+++ src/utils/constrant.js
@@ -1,6 +1,6 @@
 // export const baseUrl = "https://freshgrocery.onrender.com/api/v1";
 
-export const baseUrl = "http://15.206.173.35:4000/api/v1"; // aws url
+export const baseUrl = "https://15.206.173.35:4000/api/v1"; // aws url
 
 // // export const domain = "your-freshgrocery.netlify.app";
 
